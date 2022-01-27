c18b4bc1e88f9ac4917ecdc178832daa16f42fa1
Matthew O'Mara <tuh39725@temple.edu>
2022-01-26 22:09:09 -0500

    Add a Main class and method

---
+++ src/main/java/edu/temple/cis/paystation/Main.java
@@ -0,0 +1,9 @@
+package edu.temple.cis.paystation;
+
+public class Main {
+
+    public static void main(String[] args) throws IllegalCoinException {
+        PayStationImpl ps = new PayStationImpl();
+        ps.startInterface();
+    }
+}
