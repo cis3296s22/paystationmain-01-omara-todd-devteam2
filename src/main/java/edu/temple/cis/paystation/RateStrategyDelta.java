a4ed6aa2c1daa9d7127e798a388858f626991df4
Matthew O'Mara <tuh39725@temple.edu>
2022-02-01 10:13:25 -0500

    Implement calculate time for rate strategy delta

--- src/main/java/edu/temple/cis/paystation/RateStrategyDelta.java
+++ src/main/java/edu/temple/cis/paystation/RateStrategyDelta.java
@@ -7,11 +7,13 @@ package edu.temple.cis.paystation;
  * <p>
  * 1) Delta town will calculate time purchased based on a linear rate
  * 2) 5c buys one minute of time
+ * 3) @args the total amount of money
+ * 4) @return an integer total amount of time
  * <p>
  */
 public class RateStrategyDelta implements RateStrategy{
     @Override
     public int calculateTime(int amount){
-        return 0;
+        return amount / 5;
     }
 }
