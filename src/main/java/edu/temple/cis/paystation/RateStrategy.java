d3988df63522537c135c9145e4fb086bf2d341e7
Matthew O'Mara <tuh39725@temple.edu>
2022-02-01 10:37:01 -0500

    Refactor calculate time now returns double

--- src/main/java/edu/temple/cis/paystation/RateStrategy.java
+++ src/main/java/edu/temple/cis/paystation/RateStrategy.java
@@ -5,6 +5,7 @@ interface RateStrategy {
     /**
      * Calculate the time that will be purchased based on a specified rate strategy.
      *
+     * @return
      */
-    int calculateTime(int amount);
+    double calculateTime(int amount);
 }
